19f7cfbd80d977e23c1a283294905ccc18828a8a
MariaLight <61028743+MariaLight@users.noreply.github.com>
2024-11-21 01:30:48 +1200

    add plugins
    without node_modules

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import './index.css';
+import logo from './assets/javascript.png';
+
+
 console.log('Hello World!');
+
+const title = document.createElement('h1');
+title.textContent = 'I love JavaScript';
+const image = document.createElement('img');
+image.src = logo;
+
+[title, image].forEach(element => {
+
+    document.body.append(element);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
@@ -6,5 +7,35 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        port: 8080,
+        open: true,
+    },
     mode: 'development',
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ],
+    },
 };
